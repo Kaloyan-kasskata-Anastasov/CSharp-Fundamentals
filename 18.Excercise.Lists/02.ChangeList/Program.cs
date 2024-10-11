59599dc5eded266ec0e33f572e3820dd629e217c
kasskata <kassskata@gmail.com>
2024-10-11 19:39:06 +0300

    Some solutions fixes

--- 18.Excercise.Lists/02.ChangeList/Program.cs
+++ 18.Excercise.Lists/02.ChangeList/Program.cs
@@ -1,4 +1,10 @@
 ﻿/*
+1 2 3 4 5 5 5 6
+Delete 5
+Insert 10 1
+Delete 5
+end
+
 20 12 4 319 21 31234 2 41 23 4
 Insert 50 2
 Insert 50 5
@@ -6,31 +12,43 @@ Delete 4
 end
 */
 
+using System.Collections.Generic;
+
 internal class Program
 {
     static void Main()
     {
         List<int> list = Console.ReadLine()
-            .Split(' ')
-            .Select(int.Parse)
+            .Split()
+            .Select(e => int.Parse(e))
+            .OrderByDescending(e => e)
             .ToList();
 
-        string commands;
-        while ((commands = Console.ReadLine()) != "end")
+        string input;
+        while ((input = Console.ReadLine()) != "end")
         {
-            string[] arguments = commands.Split();
-            int index;
+            string[] arguments = input.Split();
+            int element;
 
             switch (arguments[0])
             {
                 case "Insert":
-                    int item = int.Parse(arguments[1]);
-                    index = int.Parse(arguments[2]);
-                    list.Insert(index, item);
+                {
+                    element = int.Parse(arguments[1]);
+                    int position = int.Parse(arguments[2]);
+                    list.Insert(position, element);
                     break;
+                }
                 case "Delete":
-                    item = int.Parse(arguments[1]);
-                    list.Remove(item);
+                    element = int.Parse(arguments[1]);
+                    for (int i = list.Count - 1; i >= 0; i--) 
+                    {
+                        if (list[i] == element)
+                        {
+                            list.RemoveAt(i);
+                        }
+                    }
+
                     break;
             }
         }
