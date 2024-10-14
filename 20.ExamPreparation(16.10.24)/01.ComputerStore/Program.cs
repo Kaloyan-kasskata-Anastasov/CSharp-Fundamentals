0cb0186f47e7d84e5c23290e375b07381303de30
kasskata <kassskata@gmail.com>
2024-10-14 13:04:03 +0300

    20.ExamPreparation(16.10.24)

---
+++ 20.ExamPreparation(16.10.24)/01.ComputerStore/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main()
+    {
+        decimal total = 0;
+        decimal taxes = 0;
+
+        decimal taxRate = 0.20m;
+        decimal specialDiscountRate = 0.10m;
+
+        string input;
+        while ((input = Console.ReadLine()) != "special" && input != "regular")
+        {
+            decimal partPrice = decimal.Parse(input);
+
+            if (partPrice <= 0)
+            {
+                Console.WriteLine("Invalid price!");
+                continue;
+            }
+
+            total += partPrice;
+            taxes += partPrice * taxRate;
+        }
+
+        if (total == 0)
+        {
+            Console.WriteLine("Invalid order!");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {total:F2}$");
+            Console.WriteLine($"Taxes: {taxes:F2}$");
+            Console.WriteLine("-----------");
+            
+            if (input == "special")
+            {
+                total -= (total + taxes) * specialDiscountRate;
+            }
+       
+            Console.WriteLine($"Total price: {total + taxes:F2}$");
+        }
+    }
+}
---
+++ 20.ExamPreparation(16.10.24)/03.MemoryGame/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+1 1 2 2 3 3 4 4 5 5
+1 0
+-1 0
+1 0
+1 0
+1 0
+end
+
+a 2 4 a 2 4
+0 3
+0 2
+0 1
+0 1
+end
+
+Next example == FormatException???
+
+a 2 4 a 2 4
+4 0 
+0 2 
+0 1 
+0 1 
+end
+
+*/
+
+class Program
+{
+    static void Main()
+    {
+        List<string> sequence = Console.ReadLine()
+            .Split(' ')
+            .ToList();
+
+        string input;
+        int movesCount = 0;
+        bool won = false;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            int[] indexes = input
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            movesCount++;
+            if (firstIndex == secondIndex ||
+                (firstIndex < 0 || firstIndex >= sequence.Count) ||
+                (secondIndex < 0 || secondIndex >= sequence.Count))
+            {
+                int middleIndex = sequence.Count / 2;
+                string newSymbol = $"-{movesCount}a";
+                List<string> newElements = new List<string> { newSymbol, newSymbol };
+                sequence.InsertRange(middleIndex, newElements);
+                Console.WriteLine("Invalid input! Adding additional elements to the board");
+            }
+            else
+            {
+                // hit two matching elements
+                if (sequence[firstIndex] == sequence[secondIndex])
+                {
+                    string value = sequence[firstIndex];
+                    Console.WriteLine($"Congrats! You have found matching elements - {value}!");
+
+                    if (firstIndex < secondIndex)
+                    {
+                        sequence.RemoveAt(firstIndex);
+                        sequence.RemoveAt(secondIndex - 1);
+                    }
+                    else
+                    {
+                        sequence.RemoveAt(secondIndex);
+                        sequence.RemoveAt(firstIndex - 1);
+                    }
+                }
+                // hit two different elements
+                else if (sequence[firstIndex] != sequence[secondIndex])
+                {
+                    Console.WriteLine("Try again!");
+                }
+
+                // all matching elements before he receives "end"
+                if (sequence.Count == 0)
+                {
+                    won = true;
+                    Console.WriteLine($"You have won in {movesCount} turns!");
+                    break;
+                }
+            }
+        }
+
+        // "end" before he hits all matching elements
+        if (!won)
+        {
+            Console.WriteLine("Sorry you lose :(");
+            Console.WriteLine($"{string.Join(" ", sequence)}");
+        }
+    }
+}
